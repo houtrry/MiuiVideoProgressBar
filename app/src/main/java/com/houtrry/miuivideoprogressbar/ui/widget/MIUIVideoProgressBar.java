01fe4cb16cbe8a85899f8962710fb098cc0ebede
houtrry <houtrry@hotmail.com>
2017-09-08 17:35:33 +0800

    优化代码结构

--- app/src/main/java/com/houtrry/miuivideoprogressbar/ui/widget/MIUIVideoProgressBar.java
+++ app/src/main/java/com/houtrry/miuivideoprogressbar/ui/widget/MIUIVideoProgressBar.java
@@ -7,10 +7,10 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.graphics.PointF;
 import android.support.v4.view.ViewCompat;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 import android.view.animation.AccelerateDecelerateInterpolator;
 
@@ -18,7 +18,7 @@ import android.view.animation.AccelerateDecelerateInterpolator;
  * @author houtrry
  * @version $Rev$
  * @time 2017/9/8 9:33
- * @desc ${TODO}
+ * @desc 小米视频加载时的动画
  * @updateAuthor $Author$
  * @updateDate $Date$
  * @updateDesc $TODO$
@@ -63,10 +63,10 @@ public class MIUIVideoProgressBar extends View {
     }
 
 
-    private TriangleView1 mTriangleView1_0;
-    private TriangleView1 mTriangleView1_1;
-    private TriangleView1 mTriangleView1_2;
-    private TriangleView1 mTriangleView1_3;
+    private TriangleView mTriangleView0;
+    private TriangleView mTriangleView1;
+    private TriangleView mTriangleView2;
+    private TriangleView mTriangleView3;
 
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
@@ -78,7 +78,6 @@ public class MIUIVideoProgressBar extends View {
 
         mCenterPoint = new PointF(mWidth * 0.5f, mHeight * 0.5f);
 
-
         mPointFs = new PointF[6];
         mPointFs[0] = new PointF(mCenterPoint.x - mTriangleHeight, mCenterPoint.y - mTriangleWidth * 1.0f);
         mPointFs[1] = new PointF(mCenterPoint.x - mTriangleHeight, mCenterPoint.y);
@@ -87,10 +86,10 @@ public class MIUIVideoProgressBar extends View {
         mPointFs[4] = new PointF(mCenterPoint.x, mCenterPoint.y + mTriangleWidth * 0.5f);
         mPointFs[5] = new PointF(mCenterPoint.x + mTriangleHeight, mCenterPoint.y);
 
-        mTriangleView1_0 = new TriangleView1(mPointFs[4], mPointFs[3], mPointFs[1], Color.parseColor("#B990CD"));
-        mTriangleView1_1 = new TriangleView1(mPointFs[1], mPointFs[3], mPointFs[0], Color.parseColor("#FAAA21"));
-        mTriangleView1_2 = new TriangleView1(mPointFs[3], mPointFs[4], mPointFs[5], Color.parseColor("#58B8C2"));
-        mTriangleView1_3 = new TriangleView1(mPointFs[4], mPointFs[1], mPointFs[2], Color.parseColor("#EA7A79"));
+        mTriangleView0 = new TriangleView(mPointFs[4], mPointFs[3], mPointFs[1], Color.parseColor("#B990CD"));
+        mTriangleView1 = new TriangleView(mPointFs[1], mPointFs[3], mPointFs[0], Color.parseColor("#FAAA21"));
+        mTriangleView2 = new TriangleView(mPointFs[3], mPointFs[4], mPointFs[5], Color.parseColor("#58B8C2"));
+        mTriangleView3 = new TriangleView(mPointFs[4], mPointFs[1], mPointFs[2], Color.parseColor("#EA7A79"));
     }
 
     @Override
@@ -101,13 +100,22 @@ public class MIUIVideoProgressBar extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        mTriangleView1_0.show(canvas, mPaint, progress, isReverse);
-        mTriangleView1_1.show(canvas, mPaint, progress - (isReverse ? 2 : 1), isReverse);
-        mTriangleView1_2.show(canvas, mPaint, progress - (isReverse ? 1 : 2), isReverse);
-        mTriangleView1_3.show(canvas, mPaint, progress - 3, isReverse);
+        mTriangleView0.drawTriangleView(canvas, mPaint, progress, isReverse);
+        mTriangleView1.drawTriangleView(canvas, mPaint, progress - (isReverse ? 2 : 1), isReverse);
+        mTriangleView2.drawTriangleView(canvas, mPaint, progress - (isReverse ? 1 : 2), isReverse);
+        mTriangleView3.drawTriangleView(canvas, mPaint, progress - 3, isReverse);
     }
 
-    private float progress = 1.0f;
+    /**
+     * 当前进度.
+     * (0.0~4.0)的值
+     */
+    private float progress = 0.0f;
+    /**
+     * 是否是反向.
+     * true:  反向, 表示当前进度由大减小
+     * false: 正向, 表示当前进度由小增大
+     */
     private boolean isReverse = false;
 
     public void setProgress(float progress) {
@@ -153,13 +161,111 @@ public class MIUIVideoProgressBar extends View {
     }
 
     public void clearAnimation() {
-        Log.d(TAG, "clearAnimation: ");
         if (mObjectAnimator != null && mObjectAnimator.isRunning()) {
-            Log.d(TAG, "clearAnimation: cancel");
             mObjectAnimator.cancel();
             this.clearAnimation();
             mObjectAnimator.addListener(null);
             mObjectAnimator.addUpdateListener(null);
+        }
+    }
+
+    private static class TriangleView {
+        /**
+         * 三角形的起始点.
+         * 也就是三角形慢慢变大时, 三角形的顶点.
+         */
+        private PointF startPoint;
+        /**
+         * 三角形的结束点.
+         * 三角形变小时, 三角形的顶点.
+         */
+        private PointF endPoint;
+        /**
+         * 三角形除了起始点和结束点外的第三个点.
+         */
+        private PointF thirdPoint;
+        private Path mPath = new Path();
+
+        private int color;
+
+        public TriangleView() {
+        }
+
+        public TriangleView(PointF startPoint, PointF endPoint, PointF thirdPoint, int color) {
+            this.startPoint = startPoint;
+            this.endPoint = endPoint;
+            this.thirdPoint = thirdPoint;
+            this.color = color;
+        }
+
+        public PointF getStartPoint() {
+            return startPoint;
+        }
+
+        public void setStartPoint(PointF startPoint) {
+            this.startPoint = startPoint;
+        }
+
+        public PointF getEndPoint() {
+            return endPoint;
+        }
+
+        public void setEndPoint(PointF endPoint) {
+            this.endPoint = endPoint;
+        }
+
+        public PointF getThirdPoint() {
+            return thirdPoint;
+        }
+
+        public void setThirdPoint(PointF thirdPoint) {
+            this.thirdPoint = thirdPoint;
+        }
+
+        public int getColor() {
+            return color;
+        }
+
+        public void setColor(int color) {
+            this.color = color;
+        }
+
+        public void drawTriangleView(Canvas canvas, Paint paint, float progress, boolean isReverse) {
+            progress = checkProgress(progress);
+            mPath.reset();
+            paint.setColor(color);
+            if (isReverse) {
+                mPath.moveTo(endPoint.x, endPoint.y);
+                mPath.lineTo(endPoint.x + (startPoint.x - endPoint.x) * progress, endPoint.y + (startPoint.y - endPoint.y) * progress);
+                mPath.lineTo(endPoint.x + (thirdPoint.x - endPoint.x) * progress, endPoint.y + (thirdPoint.y - endPoint.y) * progress);
+                mPath.close();
+            } else {
+                mPath.moveTo(startPoint.x, startPoint.y);
+                mPath.lineTo(startPoint.x + (endPoint.x - startPoint.x) * progress, startPoint.y + (endPoint.y - startPoint.y) * progress);
+                mPath.lineTo(startPoint.x + (thirdPoint.x - startPoint.x) * progress, startPoint.y + (thirdPoint.y - startPoint.y) * progress);
+                mPath.close();
+            }
+            canvas.drawPath(mPath, paint);
+        }
+
+        private float checkProgress(float progress) {
+            if (progress < 0) {
+                progress = 0;
+            } else if (progress > 1) {
+                progress = 1;
+            }
+            return progress;
+        }
+
+        @Override
+        public String toString() {
+            return "TriangleView{" +
+                    "startPoint=" + startPoint +
+                    ", endPoint=" + endPoint +
+                    ", thirdPoint=" + thirdPoint +
+                    ", mPath=" + mPath +
+                    ", color=" + color +
+                    '}';
         }
     }
 }
